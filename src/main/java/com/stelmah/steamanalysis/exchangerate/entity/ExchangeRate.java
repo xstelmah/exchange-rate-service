0c610b723a3b8d70170094b275fdc7321e1af178
Andrei Stselmashok <ulqstelmah@gmail.com>
2024-08-15 19:53:05 +0200

    reduced extra queries

--- src/main/java/com/stelmah/steamanalysis/exchangerate/entity/ExchangeRate.java
+++ src/main/java/com/stelmah/steamanalysis/exchangerate/entity/ExchangeRate.java
@@ -2,6 +2,7 @@ package com.stelmah.steamanalysis.exchangerate.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -40,7 +41,7 @@ public class ExchangeRate {
     @Column(nullable = false)
     private LocalDateTime timestamp;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "snapshot_id", nullable = false)
     private ExchangeRateSnapshot snapshot;
 }
