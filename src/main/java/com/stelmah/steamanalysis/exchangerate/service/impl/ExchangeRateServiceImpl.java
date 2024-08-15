6febe282ce86d2fa9c7eb24746c277635a11143a
Andrei Stselmashok <ulqstelmah@gmail.com>
2024-08-15 19:51:22 +0200

    improved logic in case if we need to swap currencies

--- src/main/java/com/stelmah/steamanalysis/exchangerate/service/impl/ExchangeRateServiceImpl.java
+++ src/main/java/com/stelmah/steamanalysis/exchangerate/service/impl/ExchangeRateServiceImpl.java
@@ -19,6 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.HttpClientErrorException;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Objects;
@@ -41,6 +42,7 @@ public class ExchangeRateServiceImpl implements ExchangeRateService {
     public ExchangeRateDto getLatestRates(String baseCurrency, String targetCurrency) {
         return exchangeRateRepository.findLatestByBaseAndTargetCurrencies(baseCurrency, targetCurrency, PageRequest.of(0, 1))
                 .map(exchangeRateMapper::toDto)
+                .map(exchangeRateDto -> swapExchangeRateIfNeeded(exchangeRateDto, baseCurrency, targetCurrency))
                 .orElseThrow(() -> new HttpClientErrorException(HttpStatus.NOT_FOUND));
     }
 
@@ -57,6 +59,22 @@ public class ExchangeRateServiceImpl implements ExchangeRateService {
         var exchangeRates = mapAndSaveExchangeRates(responseDto, snapshot);
         log.info("Fetched latest exchange rates, snapshot({}), number of rates({})",
                 snapshot.getId(), exchangeRates.size());
+    }
+
+    public ExchangeRateDto swapExchangeRateIfNeeded(ExchangeRateDto dto, String baseCurrency, String targetCurrency) {
+        // Check if the base and target currencies are swapped
+        if (targetCurrency.equals(dto.getBaseCurrency()) && baseCurrency.equals(dto.getTargetCurrency())) {
+            if (BigDecimal.ZERO.equals(dto.getRate())) {
+                log.warn("Exchange rate for {} to {} is zero, skipping this entry.",
+                        dto.getBaseCurrency(), dto.getTargetCurrency());
+                return null;
+            }
+            var adjustedRate = BigDecimal.ONE.divide(dto.getRate(), 4, RoundingMode.HALF_UP);
+            dto.setBaseCurrency(baseCurrency);
+            dto.setTargetCurrency(targetCurrency);
+            dto.setRate(adjustedRate);
+        }
+        return dto;
     }
 
     private ExchangeRateSnapshot mapAndSaveSnapshot(ExchangeRateApiResponseDto responseDto) {
